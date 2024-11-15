677ea3d698f3e3c3bb9d24182715e4ff78853a62
Liam Berry <liamandberry@gmail.com>
2024-11-15 08:16:51 +0200

    ocaml-with-variants

--- flake.nix
+++ flake.nix
@@ -10,6 +10,13 @@
           Simple Ocaml
         '';
       };
+      ocaml-with-variants = {
+        path = ./ocaml-with-variants;
+        description = "Jane street Ocaml compiler";
+        welcometext = ''
+          Jane street Ocaml compiler and repo
+        '';
+      };
 
       trivial = {
         path = ./trivial;
