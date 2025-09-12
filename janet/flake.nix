a97b64de30e6c5c8e2f7cc5fc80306999eb4a8ec
Liam Berry <liamandberry@gmail.com>
2025-09-12 10:12:32 +0200

    update inputs

--- janet/flake.nix
+++ janet/flake.nix
@@ -2,7 +2,7 @@
   description = "Description for janet project";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     janet2nix.url = "github:alan-strohm/janet2nix";
 
