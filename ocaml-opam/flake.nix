3153a115a6d450360580875ace26218bbee4497a
Liam Berry <liamandberry@gmail.com>
2026-05-27 11:16:49 +0200

    update opam-nix

--- ocaml-opam/flake.nix
+++ ocaml-opam/flake.nix
@@ -1,88 +1,78 @@
 {
-  description = "Description for ocaml project";
 
-  inputs = {
-    nixpkgs.url = "github:nix-ocaml/nix-overlays";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    opam-nix.url = "github:tweag/opam-nix";
+  nixConfig = {
+    extra-substituters = [ "https://blueberry.cachix.org" ];
+    extra-trusted-public-keys = [
+      "blueberry.cachix.org-1:bKQSogfrL/S6ceUZAkVqWl/vLc6QqUl4B8va0C7wL7k="
+    ];
   };
 
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
-
-      perSystem = {
-        config,
-        self',
-        inputs',
-        pkgs,
-        system,
-        ...
-      }: let
-        inherit (pkgs) dockerTools mkShell;
-        inherit (dockerTools) buildImage;
-        inherit (inputs.opam-nix.lib.${system}) buildOpamProject materializedDefsToScope materializeOpamProject';
-        query = {
-          ocaml-base-compiler = "*";
+  inputs = {
+    opam-nix.url = "github:tweag/opam-nix";
+    opam-repository = {
+      url = "github:ocaml/opam-repository/master";
+      flake = false;
+    };
+    opam-nix.inputs.opam-repository.follows = "opam-repository";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.follows = "opam-nix/nixpkgs";
+  };
+  outputs =
+    {
+      self,
+      flake-utils,
+      opam-nix,
+      nixpkgs,
+      ...
+    }@inputs:
+    # Don't forget to put the package name instead of `throw':
+    let
+      package = throw "change me";
+    in
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        on = opam-nix.lib.${system};
+        devPackagesQuery = {
+          # You can add "development" packages here. They will get added to the devShell automatically.
+          ocaml-lsp-server = "*";
+          ocamlformat = "*";
         };
-        # Use specific version of ocamlPackages
-        # inherit (pkgs) ocamlPackages;
-        ocamlPackages = pkgs.ocaml-ng.ocamlPackages_5_3;
-        name = "CahngeMe";
-        version = "0.0.1";
-
+        query = devPackagesQuery // {
+          ## You can force versions of certain packages here, e.g:
+          ## - force the ocaml compiler to be taken from opam-repository:
+          ocaml-base-compiler = "*";
+          ## - or force the compiler to be taken from nixpkgs and be a certain version:
+          # ocaml-system = "4.14.0";
+          ## - or force ocamlfind to be a certain version:
+          # ocamlfind = "1.9.2";
+        };
+        scope = on.buildOpamProject' { } ./. query;
         overlay = final: prev: {
-          "${name}" = prev.${name}.overrideAttrs (_: {
-            # override derivation attributes, e.g. add additional dependacies
-            buildInputs = [];
+          # You can add overrides here
+          ${package} = prev.${package}.overrideAttrs (_: {
+            # Prevent the ocaml dependencies from leaking into dependent environments
+            doNixSupport = false;
           });
         };
+        scope' = scope.overrideScope overlay;
+        # The main package containing the executable
+        main = scope'.${package};
+        # Packages from devPackagesQuery
+        devPackages = builtins.attrValues (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
+      in
+      {
+        legacyPackages = scope';
 
-        resolved-scope = let
-          scope = buildOpamProject {} name ./. query;
-        in
-          scope.overrideScope' overlay;
-        materialized-scope = let
-          scope =
-            materializedDefsToScope
-            {sourceMap.${name} = ./.;}
-            ./package-defs.json;
-        in
-          scope.overrideScope' overlay;
-      in rec {
-        devShells = {
-          default = mkShell {
-            inputsFrom = [self'.packages.default];
-            buildInputs = with ocamlPackages; [
-              utop
-              ocamlformat
-              # patch ocaml-lsp so that inlay hints dont hide ghost values
-              (ocamlPackages.ocaml-lsp.overrideAttrs (oldAttrs: {
-                patches = [
-                  ./inlay-hints.patch
-                ];
-              }))
-            ];
-          };
+        packages.default = main;
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ main ];
+          buildInputs = devPackages ++ [
+            # You can add packages from nixpkgs here
+          ];
         };
-
-        packages = {
-          resolved = resolved-scope;
-          materialized = materialized-scope;
-          package-defs = materializeOpamProject' {} ./. query;
-          default = materialized-scope.${name};
-
-          docker = buildImage {
-            inherit name;
-            tag = version;
-            config = {
-              Cmd = ["${self'.packages.default}/bin/${name}"];
-              Env = [
-                "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-              ];
-            };
-          };
-        };
-      };
-    };
+      }
+    );
 }
